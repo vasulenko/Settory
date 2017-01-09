dcc0d83bc56c4a05fceaafa84672381a458b1356
vitalii_denysiuk <dendoc97@gmail.com>
2017-01-09 17:05:13 +0200

    fix bugs 2

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
- $(function(){
- $('.picture').height($('.picture').width());
-});
-$(window).resize(function(){        $('.picture').height($('.picture').width());
-});
+//  $(function(){
+//  $('.picture').height($('.picture').width());
+// });
+// $(window).resize(function(){        $('.picture').height($('.picture').width());
+// });
 
 $(window).scroll(function() {
     if ($(window).scrollTop() >= 15){
-        $('header').addClass('header--hidden'); 
+        $('header').addClass('header--hidden');
     } else {
         $('header').removeClass('header--hidden');
-    } 
+    }
 })
    $(document).ready(function(){
       $('.bxslider').bxSlider({
